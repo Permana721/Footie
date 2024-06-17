fea30943931c9ae29108bb2a35ca5bf0296d49ae
Permana <sdharma721@gmail.com>
2024-06-17 15:08:19 +0700

    ubah like jadi realtime

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -8,27 +8,26 @@ use Illuminate\Http\Request;
 
 class LikeController extends Controller
 {
-    
-public function like(Request $request, $id)
-{
-    $user_id = auth()->user()->id;
-    $existing_like = Like::where('product_id', $id)
-                        ->where('user_id', $user_id)
-                        ->first();
+    public function like(Request $request, $id)
+    {
+        $user_id = auth()->user()->id;
+        $existing_like = Like::where('product_id', $id)
+                            ->where('user_id', $user_id)
+                            ->first();
 
-    if ($existing_like) {
-        $existing_like->delete();
-    } else {
-        Like::create([
-            'product_id' => $id,
-            'user_id' => $user_id
-        ]);
+        $liked = false;
+        if ($existing_like) {
+            $existing_like->delete();
+        } else {
+            Like::create([
+                'product_id' => $id,
+                'user_id' => $user_id
+            ]);
+            $liked = true;
+        }
+
+        $totalLikes = Like::where('product_id', $id)->count();
+
+        return response()->json(['likes' => $totalLikes, 'liked' => $liked]);
     }
-
-    $totalLikes = Like::where('product_id', $id)->count();
-
-    $liked = $existing_like ? true : false;
-
-    return response()->json(['likes' => $totalLikes, 'liked' => $liked]);
-}
 }
