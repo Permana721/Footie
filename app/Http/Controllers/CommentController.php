7f61d208f5372438a908b40d4e4cbbce5ca5eb77
Permana <sdharma721@gmail.com>
2024-06-17 17:29:07 +0700

    update realtime comment

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -8,32 +8,42 @@ use Illuminate\Http\Request;
 class CommentController extends Controller
 {
     public function store(Request $request)
-    {
-        $request->validate([
-            'comment' => 'required|string',
-            'product_id' => 'required|exists:products,id',
-        ]);
+{
+    $request->validate([
+        'comment' => 'required|string',
+        'product_id' => 'required|exists:products,id',
+    ]);
 
-        $comment = new Comment();
-        $comment->user_id = auth()->id();
-        $comment->product_id = $request->product_id;
-        $comment->user_name = auth()->user()->name;
-        $comment->user_foto = auth()->user()->foto;
-        $comment->komentar = $request->comment;
-        $comment->save();
+    $comment = new Comment();
+    $comment->user_id = auth()->id();
+    $comment->product_id = $request->product_id;
+    $comment->user_name = auth()->user()->name;
+    $comment->user_foto = auth()->user()->foto;
+    $comment->komentar = $request->comment;
+    $comment->save();
 
-        return back();
-    }
+    return response()->json(['comment' => $comment]);
+}
 
-    public function update(Request $request, $id) {
-        $comment = Comment::findOrFail($id);
-        $comment->komentar = $request->input('comment');
-        $comment->save();
-    
-        return redirect()->back();
-    }
+public function update(Request $request, $id)
+{
+    $request->validate([
+        'comment' => 'required|string',
+    ]);
 
-    
+    $comment = Comment::findOrFail($id);
+    $comment->komentar = $request->comment;
+    $comment->save();
+
+    // Check if comment was edited
+    $edited = $comment->wasChanged();
+
+    return response()->json([
+        'comment' => $comment,
+        'edited' => $edited
+    ]);
+}
+
 public function destroy(Comment $comment)
 {
     if ($comment->user_id !== auth()->id()) {
