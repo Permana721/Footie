6b82ac19bcbd8e55d4ef503db112976d4f61a0f5
Permana <sdharma721@gmail.com>
2024-12-16 22:42:39 +0700

    update

---
+++ storage/framework/views/f3045b88105bddddf4f7569618cf3c23.php
@@ -0,0 +1,51 @@
+<footer class="section-p1">
+    <div class="col">
+        <img src="/assets/img/logo/logo.png" alt="" style="visibility: hidden;">
+        <div class="lp">
+            <h4>Layanan Pelanggan</h4>
+            <p>Bantuan</p>
+            <p>Metode Pembayaran</p>
+            <p>Laporkan Bug</p>
+            <p>Pengembalian Barang & Dana</p>
+        </div>
+        <div class="follow">
+            <h4>Ikuti Kami</h4>
+            <div class="icon">
+                <ion-icon name="logo-facebook" onclick="fb()"></ion-icon>
+                <ion-icon name="logo-instagram"></ion-icon>
+                <ion-icon name="logo-twitter"></ion-icon>
+                <ion-icon name="logo-youtube" onclick="yt()"></ion-icon>
+            </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <h4>Footie</h4>
+        <a href="<?php echo e(url('/about')); ?>" target="_blank">Tentang Footie</a>
+        <a href="https://www.tokopedia.com/terms?lang=id" target="_blank">Kebijakan Layanan</a>
+        <a href="https://www.tokopedia.com/privacy?lang=id" target="_blank">Kebijakan Privasi</a>
+        <a href="https://www.tokopedia.com/blog/" target="_blank">Blog</a>
+        <a href="https://www.tokopedia.com/mitra" target="_blank">Mitra</a>
+    </div>
+    
+    <div class="col">
+        <h4>Akun Saya</h4>
+        <a data-bs-toggle="modal" data-bs-target="#exampleModal" style="cursor: pointer">Masuk</a>
+        <a data-bs-toggle="modal" data-bs-target="#registerModal" style="cursor: pointer;">Daftar</a>
+        <a href="https://www.tokopedia.com/cart" target="_blank">Keranjang Belanja</a>
+        <a href="https://www.tokopedia.com/order-list?status=dikirim" target="_blank">Lacak Pesanan Saya</a>
+        <a href="https://www.tokopedia.com/user/settings" target="_blank">Ubah Alamat Email</a>
+    </div>
+    
+
+    <div class="col install">
+        <h4>Dapatkan Aplikasi Footie</h4>
+        <p>Install Dari App Store atau Play Store</p>
+        <div class="row">
+            <img src="/assets/img/bayar/app.jpg" alt="">
+            <img src="/assets/img/bayar/play.jpg" alt="" class="play">
+        </div>
+        <p>Pembayaran </p>
+        <img src="/assets/img/bayar/pay.png" alt="" class="bayar">
+    </div>
+</footer><?php /**PATH Z:\laragon\www\Footie\resources\views/pelanggan/componen/footer.blade.php ENDPATH**/ ?>
