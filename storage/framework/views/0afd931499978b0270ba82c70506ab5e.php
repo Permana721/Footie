34f9ee1a98ac19f3230d57eb9eca5fe16d3d7d7d
Permana <sdharma721@gmail.com>
2024-12-18 20:23:30 +0700

    update

---
+++ storage/framework/views/0afd931499978b0270ba82c70506ab5e.php
@@ -0,0 +1,104 @@
+
+
+<?php $__env->startSection('content'); ?>
+<body>
+      <div class="bg-light">
+        <div class="container py-5">
+          <div class="row h-100 align-items-center py-5">
+            <div class="col-lg-6">
+              <h1 class="display-4">Tentang Footie</h1>
+              <p class="lead text-muted mb-0">Kami adalah penyedia solusi kreatif dan inovatif dalam pembuatan website e-commerce yang menghubungkan Anda dengan pelanggan dalam industri makanan dan minuman.</p>
+              </p>
+            </div>
+            <div class="col-lg-6 d-none d-lg-block"><img src="https://bootstrapious.com/i/snippets/sn-about/illus.png" alt="" class="img-fluid"></div>
+          </div>
+        </div>
+      </div>
+      <div class="bg-white py-5">
+        <div class="container py-5">
+          <div class="row align-items-center mb-5">
+            <div class="col-lg-6 order-2 order-lg-1"></i>
+              <h2 class="font-weight-light">Visi dan Misi Kami</h2>
+              <p class="font-italic text-muted mb-4">Visi kami adalah menjadi pionir dalam membawa rasa dan kualitas terbaik ke meja Anda, sementara misi kami adalah memberikan pengalaman belanja online yang tak tertandingi dengan pilihan makanan dan minuman yang berkualitas tinggi.</p>
+            </div>
+            <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2"><img src="https://bootstrapious.com/i/snippets/sn-about/img-1.jpg" alt="" class="img-fluid mb-4 mb-lg-0"></div>
+          </div>
+          <div class="row align-items-center">
+            <div class="col-lg-5 px-5 mx-auto"><img src="<?php echo e(asset('assets/img/banner/22.png')); ?>" alt="" class="img-fluid mb-4 mb-lg-0"></div>
+            <div class="col-lg-6">
+              <h2 class="font-weight-light">Komitmen Terhadap Kualitas dan Harga </h2>
+              <p class="font-italic text-muted mb-4">Kami berkomitmen untuk menyediakan harga terbaik dan kualitas premium dalam setiap produk makanan dan minuman yang kami tawarkan, menjadikan kepuasan pelanggan sebagai prioritas utama kami.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="bg-light py-5">
+        <div class="container py-5">
+          <div class="row mb-4">
+            <div class="col-lg-5">
+              <h2 class="display-4 font-weight-light">Tim Kami</h2>
+              <p class="font-italic text-muted inline-flex">Terkumpul dari individu berbakat yang berkomitmen tinggi</p>
+            </div>
+          </div>
+      
+          <div class="row text-center">
+            <div class="col-xl-3 col-sm-6 mb-5">
+              <div class="bg-white rounded shadow-sm py-5 px-4"><img src="<?php echo e(asset('assets/img/profil/1.png')); ?>" alt="" width="99" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm circle-image">
+                <h5 class="mb-0">A Azka</h5><span class="small text-uppercase text-muted">Ketua - CEO</span>
+                <ul class="social mb-0 list-inline mt-3">
+                  <li class="list-inline-item"><a class="social-link"><i class="fa-brands fa-facebook" onclick="fb()"></i></a></li>
+                  <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-solid fa-envelope"></i></a></li>
+                  <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-instagram"></i></a></li>
+                  <li class="list-inline-item"><a class="social-link"><i class="fa-brands fa-youtube" onclick="yt_piko()"></i></a></li>
+                </ul>
+              </div>
+            </div>
+      
+            <div class="col-xl-3 col-sm-6 mb-5">
+              <div class="bg-white rounded shadow-sm py-5 px-8"><img src="<?php echo e(asset('assets/img/profil/2.png')); ?>" alt="" width="119" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                <h5 class="mb-0">Ihsan</h5><span class="small text-uppercase text-muted">Anggota - Front End</span>
+                <ul class="social mb-0 list-inline mt-3">
+                  <ul class="social mb-0 list-inline mt-3">
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-facebook"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-solid fa-envelope"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-instagram"></i></a></li>
+                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-youtube"></i></a></li>
+                  </ul>
+                </ul>
+              </div>
+            </div>
+      
+            <div class="col-xl-3 col-sm-6 mb-5">
+              <div class="bg-white rounded shadow-sm py-5 px-4"><img src="<?php echo e(asset('assets/img/profil/3.png')); ?>" alt="" width="90" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                <h5 class="mb-0">Alvino</h5><span class="small text-uppercase text-muted">Anggota - Back End</span>
+                <ul class="social mb-0 list-inline mt-3">
+                  <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-facebook"></i></a></li>
+                  <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-solid fa-envelope"></i></a></li>
+                  <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-instagram"></i></a></li>
+                  <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-youtube"></i></a></li>
+                </ul>
+              </div>
+            </div>
+      
+            <div class="col-xl-3 col-sm-6 mb-5">
+              <div class="bg-white rounded shadow-sm py-5 px-4"><img src="<?php echo e(asset('assets/img/profil/5.png')); ?>" alt="" width="85" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                <h5 class="mb-0">Permana</h5><span class="small text-uppercase text-muted">Anggota - Full Stack</span>
+                <ul class="social mb-0 list-inline mt-3">
+                  <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-facebook"></i></a></li>
+                  <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-solid fa-envelope"></i></a></li>
+                  <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-instagram"></i></a></li>
+                  <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-youtube"></i></a></li>
+                </ul>
+              </div>
+            </div>
+      
+          </div>
+        </div>
+      </div>
+
+      <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-K6u1I/oqD6Km6FzgiujZmds5enZvIsPTVr4jQl8rO94=" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-pzjw8f+UA0FZ6pi+JXJKJ6vBkkjbKs0Gikdb6X8k5yqqls/Cv/Z6+Pip1k0Xamf7" crossorigin="anonymous"></script>
+</body>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('pelanggan.layout.index', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH Z:\laragon\www\Footie\resources\views/pelanggan/page/about.blade.php ENDPATH**/ ?>
